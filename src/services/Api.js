6e5b493fd04b453e0fa0db892b8db0c48c3cf9c2
snehakaradia <snehakaradia430206@gmail.com>
2023-12-19 15:42:08 +0530

    initial commit

---
+++ src/services/Api.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const API_KEY = '82b945c4bf1131a294fae54891677377';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+export const getWeather =async (city, country) => {
+    try {
+      let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+      return response.data;
+    } catch (error) {
+        console.log('Error while calling the api',error.message);
+        return error.response;
+    }
+}
